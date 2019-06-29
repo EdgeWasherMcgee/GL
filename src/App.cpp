8e33802924d3814971c35a193a6ac86c4ab32ec1
pythereum <axel.ingvast@gmail.com>
2019-06-29 16:46:37 +0200

    Initial commit

---
+++ src/App.cpp
@@ -0,0 +1,193 @@
+#include <GL/glew.h>
+#include <GLFW/glfw3.h>
+
+#include <stdio.h>
+#include <iostream>
+#include <fstream>
+#include <sstream>
+#include "Renderer.h"
+
+#include "VertexBuffer.h"
+#include "IndexBuffer.h"
+#include "VertexArray.h"
+
+struct ShaderProgramSource {
+    std::string VertexSource;
+    std::string FragmentSource;
+};
+
+static ShaderProgramSource ParseShader(const std::string& filepath) {
+
+    std::ifstream stream(filepath);
+    if (stream.good() == false) {
+        std::cout << "Something went wrong while Parsing Shader" << std::endl;
+        ASSERT(stream.good());
+    }
+
+    std::string line;
+    std::stringstream ss[2];
+
+    enum class ShaderType {
+        NONE = -1, VERTEX = 0, FRAGMENT = 1
+    };
+
+    ShaderType type = ShaderType::NONE;
+
+
+    while (getline(stream, line)) {
+        if (line.find("#shader") != std::string::npos) {
+            if (line.find("vertex") != std::string::npos) {
+                type = ShaderType::VERTEX;
+            }
+
+            else if (line.find("fragment") != std::string::npos) {
+                type = ShaderType::FRAGMENT;
+            }
+
+        } else {
+            ss[(int)type] << line << "\n";
+        }
+
+    }
+
+    return {ss[0].str(), ss[1].str()};
+
+}
+
+static unsigned int CompileShader(unsigned int type, const std::string& source) {
+
+    GLCall(unsigned int shader = glCreateShader(type));
+    const char* src = source.c_str();
+    GLCall(glShaderSource(shader, 1, &src, nullptr));
+    GLCall(glCompileShader(shader));
+
+    int result;
+    GLCall(glGetShaderiv(shader, GL_COMPILE_STATUS, &result));
+    if (result == GL_FALSE) {
+        int length;
+        GLCall(glGetShaderiv(shader, GL_INFO_LOG_LENGTH, &length));
+        char *message = new char[length];
+        std::cout << "Failed to compile " << (type == GL_VERTEX_SHADER ? "vertex" : "fragment") << "shader" << std::endl;
+        std::cout << message << std::endl;
+        GLCall(glDeleteShader(shader));
+        return 0;
+    }
+
+    return shader;
+}
+
+
+static unsigned int CreateShader(const std::string& vertexShader, const std::string& fragmentShader) {
+    GLCall(unsigned int program = glCreateProgram());
+    unsigned int vs = CompileShader(GL_VERTEX_SHADER, vertexShader);
+    unsigned int fs = CompileShader(GL_FRAGMENT_SHADER, fragmentShader);
+
+    GLCall(glAttachShader(program, vs));
+    GLCall(glAttachShader(program, fs));
+
+    GLCall(glLinkProgram(program));
+    GLCall(glValidateProgram(program));
+    GLCall(glDeleteShader(vs));
+    GLCall(glDeleteShader(fs));
+    return program;
+}
+
+
+int main(void) {
+    GLFWwindow* window;
+
+    /* Initialize the library */
+    if (!glfwInit())
+        return -1;
+
+    /* Create a windowed mode window and its OpenGL context */
+    window = glfwCreateWindow(640, 480, "glProg", NULL, NULL);
+    if (!window) {
+        glfwTerminate();
+        return -1;
+    }
+
+    glfwWindowHint(GLFW_CONTEXT_VERSION_MAJOR, 3);
+    glfwWindowHint(GLFW_CONTEXT_VERSION_MINOR, 3);
+    glfwWindowHint(GLFW_OPENGL_PROFILE, GLFW_OPENGL_CORE_PROFILE);
+
+    glfwMakeContextCurrent(window);
+
+    GLenum err = glewInit();
+    if (GLEW_OK != err) {
+    /* Problem: glewInit failed, something is seriously wrong. */
+        fprintf(stderr, "Error: %s\n", glewGetErrorString(err));
+    }
+
+
+
+    float positions[8] = {
+    	-0.5f, -0.5f,
+    	 0.5f, -0.5f,
+    	 0.5f,  0.5f,
+        -0.5f,  0.5f
+    };
+
+    unsigned int indices[6] = {
+        0, 1, 2,
+        2, 3, 0
+    };
+
+    VertexArray va;
+    VertexBuffer vb = VertexBuffer(positions, sizeof(float) * 8);
+
+    VertexBufferLayout layout;
+    layout.Push(2, GL_FLOAT);
+    va.AddBuffer(vb, layout);
+
+    IndexBuffer ib(indices, 6);
+
+    ShaderProgramSource source = ParseShader("../res/shaders/Basic.shader");
+    unsigned int program = CreateShader(source.VertexSource, source.FragmentSource);
+    GLCall(glUseProgram(program))
+
+    GLCall(int location = glGetUniformLocation(program, "u_Color"));
+    ASSERT(location != -1);
+    GLCall(glUniform4f(location, 0.8f, 0.3f, 0.8f, 1.0f))
+
+    GLCall(glBindVertexArray(0));
+    GLCall(glUseProgram(0));
+    GLCall(glBindBuffer(GL_ARRAY_BUFFER, 0));
+    GLCall(glBindBuffer(GL_ELEMENT_ARRAY_BUFFER, 0));
+
+    // float r = 0.0f;
+    // float increment = 0.05f;
+
+    /* Loop until the user closes the window */
+    while (!glfwWindowShouldClose(window)) {
+        /* Render here */
+        GLCall(glClear(GL_COLOR_BUFFER_BIT));
+
+        GLCall(glUseProgram(program));
+        GLCall(glUniform4f(location, 0.2f, 0.3f, 0.5f, 1.0f));
+
+        va.Bind();
+        ib.Bind();
+
+        std::cout << "In loop" << std::endl;
+
+        GLCall(glDrawElements(GL_TRIANGLES, 6, GL_UNSIGNED_INT, nullptr));
+
+        // if (r > 1.0f or r < 0.0f) {
+        //     increment *= -1;
+        // }
+
+        // r += increment;
+
+        /* Swap front and back buffers */
+        glfwSwapBuffers(window);
+
+        /* Poll for and process events */
+        glfwPollEvents();
+    }
+
+    glDeleteProgram(program);
+
+    glfwTerminate();
+    return 0;
+}
---
+++ src/Renderer.cpp
@@ -0,0 +1,16 @@
+#include <GL/glew.h>
+#include <iostream>
+#include "Renderer.h"
+
+void GLClearErrors() {
+    while(glGetError() != GL_NO_ERROR);
+}
+
+bool GLLogCall(const char* function, const char* file, int line) {
+
+    while(GLenum error = glGetError()) {
+        std::cout << "[OpenGL error] (0x" << std::hex << error << ")" << " in file " << file << " in function " << function << " on line " << line << std::endl;
+        return false;
+    }
+    return true;
+}
